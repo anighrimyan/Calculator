29a8215f5bc9d0e2f691758d607ad3a6d69452c8
Ani Ghrimyan <89627723+anighrimyan@users.noreply.github.com>
2022-11-25 13:30:45 +0400

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,32 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+#include <QLabel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Calculator; }
+QT_END_NAMESPACE
+
+class Calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Calculator(QWidget *parent = nullptr);
+    ~Calculator();
+
+private:
+    Ui::Calculator *ui;
+
+private:
+    QLabel *Display;
+
+private slots:
+   void NumPressed();
+   void MathButtonPressed();
+   void EqualButtonPressed();
+   void ClearAll();
+   void Point();
+};
+#endif // CALCULATOR_H
